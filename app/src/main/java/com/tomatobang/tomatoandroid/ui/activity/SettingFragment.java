6fae5ad4b260b89f51b47c0b4c23e8d86928189c
kobepeng <pengkobe123@gmail.com>
2017-06-22 16:58:09 +0800

    add tab layout demo from cnblogs

---
+++ app/src/main/java/com/tomatobang/tomatoandroid/ui/activity/SettingFragment.java
@@ -0,0 +1,22 @@
+package com.tomatobang.tomatoandroid.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.tomatobang.tomatoandroid.R;
+
+/**
+ *
+ * Created by caobotao on 16/1/4.
+ */
+public class SettingFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab4, container, false);
+        return view;
+    }
+}
